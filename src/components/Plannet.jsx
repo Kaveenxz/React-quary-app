d72dd790c5b0fce4dcc35597ea39e927accb763e
Kaveenxz <kaveenhansithx@gmail.com>
2024-05-31 17:41:28 +0530

    Added react-query dependency and implemented it in Plannet component and index.js

--- src/components/Plannet.jsx
+++ src/components/Plannet.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+
+const fetchPlannets = async ()=>{
+    const res =  await fetch('https://swapi.dev/api/planets/');
+    return res.json();
+}
 
 function Plannet() {
+    const {data, status} = useQuery('plannets', fetchPlannets)
+    console.log(data)
   return (
     <div>Plannet</div>
   )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const quaryClient = new QueryClient()
+
 root.render(
+  <QueryClientProvider client={quaryClient}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </QueryClientProvider>
 );
